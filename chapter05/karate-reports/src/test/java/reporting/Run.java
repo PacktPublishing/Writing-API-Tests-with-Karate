ad79f5f79e38cc566a67e5a2e4d001a1316efbbd
bischoffdev <benjamin.bischoff@trivago.com>
2022-11-25 16:43:53 +0100

    report example

---
+++ chapter05/karate-reports/src/test/java/reporting/Run.java
@@ -0,0 +1,17 @@
+package reporting;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class Run {
+
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:reporting")
+            .parallel(2);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+
+}
