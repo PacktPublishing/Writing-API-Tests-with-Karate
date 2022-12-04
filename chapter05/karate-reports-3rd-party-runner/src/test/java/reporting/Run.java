a72d3fba26ea9bd38e54fb1ea8d78e9ccb884cf8
Benjamin Bischoff <benjamin.bischoff@gmx.de>
2022-12-04 20:16:28 +0100

    cluecumber maven and core

--- chapter05/karate-reports-3rd-party-runner/src/test/java/reporting/Run.java
+++ chapter05/karate-reports-3rd-party-runner/src/test/java/reporting/Run.java
@@ -2,19 +2,27 @@ package reporting;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import com.trivago.cluecumber.core.CluecumberCore;
+import com.trivago.cluecumber.engine.exceptions.CluecumberException;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class Run {
 
     @Test
-    void testParallel() {
+    void testParallel() throws CluecumberException{
         Results results = Runner.path("classpath:reporting")
             .outputCucumberJson(true)
-            .outputJunitXml(false)
             .reportDir("target/myReport")
             .outputHtmlReport(false)         
             .parallel(2);
+
+        new CluecumberCore.Builder().build()
+            .generateReports(
+                "target/myReport",
+                "target/customReport");
+
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 
