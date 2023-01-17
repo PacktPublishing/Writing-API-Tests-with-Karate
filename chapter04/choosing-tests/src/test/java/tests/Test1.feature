bc884cc893a27a2e3da7dac13b9aa7bed8314628
Benjamin Bischoff <benjamin.bischoff@gmx.de>
2023-01-17 23:15:48 +0100

    updated examples

--- chapter04/choosing-tests/src/test/java/tests/Test1.feature
+++ chapter04/choosing-tests/src/test/java/tests/Test1.feature
@@ -3,6 +3,9 @@ Feature: Feature 1
   Scenario: Feature 1 Scenario 1
     * print "Feature 1 Scenario 1"
 
-  @ignore
   Scenario: Feature 1 Scenario 2
     * print "Feature 1 Scenario 2"
+
+  @ignore
+  Scenario: Ignored scenario
+    * print "Ignored scenario"
--- chapter07/java/src/test/java/db/MySQL.java
+++ chapter07/java/src/test/java/db/MySQL.java
@@ -9,23 +9,25 @@ import com.intuit.karate.JsonUtils;
 
 public class MySQL {  
     public String getMagicians() throws Exception {
-        String connectionString = "jdbc:mysql://sql7.freesqldatabase.com/sqlxxxxxxx";
-        String user = "sqlxxxxxxx";
-        String password = "xxxxxxxxxx";
+        String connectionString =
+            "jdbc:mysql://sql7.freesqldatabase.com/sqlxxx";
+        String user = "sqlxxx";
+        String password = "xxx";
         try (
-                Connection connect = DriverManager.getConnection(connectionString, user, password);
-                Statement statement = connect.createStatement();
-                ResultSet resultSet = statement.executeQuery("select * from magicians")
+            Connection c = DriverManager.getConnection(
+                connectionString, user, password
+            );
+            Statement s = c.createStatement();
+            ResultSet result = 
+                s.executeQuery("select * from magicians")
         ) {
             List<Magician> magicians = new ArrayList<>();
-            while (resultSet.next()) {
-                String name = resultSet.getString("name");
-                int birthyear = resultSet.getInt("birthyear");
+            while (result.next()) {
+                String name = result.getString("name");
+                int birthyear = result.getInt("birthyear");
                 magicians.add(new Magician(name, birthyear));
             }
             return JsonUtils.toJson(magicians);
-        } catch (Exception e) {
-            throw e;
         }
     }
 }
