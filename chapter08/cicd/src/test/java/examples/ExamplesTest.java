3d092f88a6b315c0b4c9e3e59ea9c88b75469df8
Benjamin Bischoff <benjamin.bischoff@gmx.de>
2023-02-18 00:02:56 +0100

    report

--- chapter08/cicd/src/test/java/examples/ExamplesTest.java
+++ chapter08/cicd/src/test/java/examples/ExamplesTest.java
@@ -2,15 +2,27 @@ package examples;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import com.trivago.cluecumber.core.CluecumberCore;
+import com.trivago.cluecumber.engine.exceptions.CluecumberException;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class ExamplesTest {
 
     @Test
-    void testParallel() {
+    void testParallel() throws CluecumberException {
         Results results = Runner.path("classpath:examples")
-                .parallel(1);
+            .outputCucumberJson(true)
+            .reportDir("target/myReport")
+            .outputHtmlReport(false)         
+            .parallel(1);
+
+                new CluecumberCore.Builder().build()
+                    .generateReports(
+                        "target/myReport",
+                        "target/customReport");                
+
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 
