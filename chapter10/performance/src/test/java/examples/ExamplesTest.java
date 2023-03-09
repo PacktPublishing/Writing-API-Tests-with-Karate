bb6da261e7d712ac4f6134e58397978c0f43c0d6
bischoffdev <benjamin.bischoff@trivago.com>
2023-03-09 14:45:40 +0100

    placeholder project

---
+++ chapter10/performance/src/test/java/examples/ExamplesTest.java
@@ -0,0 +1,17 @@
+package examples;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ExamplesTest {
+
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:examples")
+                .parallel(1);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+
+}
